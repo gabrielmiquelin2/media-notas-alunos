b9ecb5ba220836db3e45c79c194d5d88caf6b0e5
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2022-08-19 14:58:35 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+console.log('Inicio da aplicação')
+
+const avaliacao1 = 4
+const avaliacao2 = 8
+let avaliacaoOptativa = 0
+
+if (avaliacaoOptativa == undefined){
+   avaliacaoOptativa = -1
+   console.log(avaliacaoOptativa)
+}
